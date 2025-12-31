08f3ab8b7855271191ec784237a59968f1f360e8
Raphael Oliveira <raphael.msoliveira@gmail.com>
2025-12-31 18:41:59 -0300

    feat(dot): add initial dotfiles

---
+++ .zshrc
@@ -0,0 +1,33 @@
+# export PATH
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
+
+# export environment variables 
+export EDITOR='nvim'
+
+
+# History 
+HISTFILE=~/.zsh_history
+HISTSIZE=10000000
+SAVEHIST=10000000
+
+setopt APPEND_HISTORY
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY
+
+alias cd="z"
+alias zj="zellij"
+alias n="nvim"
+alias ee="nvim ~/Dev/dotfiles/packages.sh"
+alias ii="sudo ~/Dev/dotfiles/packages.sh"
+alias hh="nvim ~/.config/hypr/hyprland.conf"
+alias ls="eza --icons"
+
+eval "$(starship init zsh)"
+eval "$(zoxide init zsh)"
+source <(fzf --zsh)
+
---
+++ arch_packages.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+echo "updating"
+sudo pacman -Syu --noconfirm
+
+PACKAGES=(
+  neovim
+  git
+  btop
+  curl
+  wget
+  fzf
+  less
+  fd
+  yazi
+  power-profiles-daemon
+  openssh
+  lazygit
+  stow
+  fastfetch
+  starship
+  base-devel
+  man
+  tealdeer
+  zoxide
+  zsh
+  eza
+  ripgrep
+  7zip
+  ntfs-3g
+)
+
+echo "installing packages"
+sudo pacman -S --needed --noconfirm "${PACKAGES[@]}"
+
+TOOLS=(
+  gparted
+  freecad
+  geogebra
+  libreoffice-fresh
+  steam #multilib
+  gamescope
+  blender
+  darktable
+  gimp
+  deluge
+  deluge-gtk
+  kdenlive
+  mpv
+  obs-studio
+  inkscape
+  calibre
+  pdfarranger
+  kicad
+  gnome-boxes
+  ncdu
+)
+
+AUR=(
+  anki
+)
